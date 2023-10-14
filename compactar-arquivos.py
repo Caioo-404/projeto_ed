60e6e023101495b3ce945362fa5262e17b61265f
Caioo-404 <caiooliveira0277@gmail.com>
2023-10-14 19:10:38 -0300

    criando class nodeHuffman / introdução de manipulação de arquivos

--- compactar-arquivos.py
+++ compactar-arquivos.py
@@ -1,1 +1,21 @@
-print("hello word")
+#Criando uma classe de nos huffman
+class node:
+    #Construtor
+    def __init__(self, valor, frequecia):
+        self.char = valor
+        self.freq = frequecia
+        self.left = None
+        self.right = None
+
+def contrucaoArvore(text):
+    #Ver frequência de cada caracter e colocar em um dict:
+    frequencia = dict()
+    for char in text:
+        if char in frequencia:
+            frequencia[char] += 1
+        else:
+            frequencia[char] = 1
+
+    #Criar uma lista de nó-huffman (valor e frequência)
+    listaNo = [node(char, freq) for char, freq in frequencia.items()]
+    #Pro resto aprender a usar heap
---
+++ manipular-arquivos.py
@@ -0,0 +1,29 @@
+'''
+--Códigos para arquivos:
+"r" -> Usar para ler
+"w" -> Usar para escrever
+"r+" -> Ler e Escrever
+"a" -> acrescentar algo (não apaga tudo como o "W")
+"x" -> cria
+*Modo texto ("t")
+*Modo binário ("b")
+Ex: "wb" representa escrever em modo binário.
+
+
+--Arquivo:
+open("nome_arquivo", "tipo do que for fazer")
+arq.close() -> Fecha o arquivo
+arq.write("texto") -> escrever
+arq.readlines() -> retorna uma lista com o texto
+arq.read() -> normal
+
+
+--Estrutura Arquivos(Fecha auto)
+with open('nome_do_arquivo.txt', "w") as arquivo:
+    ...código...
+
+    
+--Excluir arquivo:
+import os
+os.remove("sample_file.txt")
+'''
