a5808df228087e4ab8600affd52a0fce2a94defe
Caioo-404 <caiooliveira0277@gmail.com>
2023-10-28 22:05:09 -0300

    compactar e criar dict

--- compactar-arquivos.py
+++ compactar-arquivos.py
@@ -1,7 +1,7 @@
 import heapq
 
 #Criando uma classe de nós huffman
-class node:
+class Node:
     #Construtor
     def __init__(self, valor, frequecia):
         self.char = valor
@@ -9,15 +9,19 @@ class node:
         self.left = None
         self.right = None
 
-    #Representação em string (ajuda na visualização da heap)
-    def __repr__(self):
-        return "( " + str(self.char) + " )"
+    #Representação em string (ajudar na visualização)
+    def __str__(self):
+        return "(" + str(self.char) + "," + str(self.freq) + ")"
     
-    #Comparar na class
-    def __lt__ (self, outra):
-        return self.freq < outra.freq
+    #Comparar na class (< e ==)
+    def __lt__(self, outro):
+        return self.freq < outro.freq
+    
+    def __eq__(self, outro ):
+        return self.freq == outro.freq
+        
 
-
+#Construir árvore huffman 
 def contrucaoArvore(text):
     #Ver frequência de cada caracter e colocar em um dict:
     frequencia = dict()
@@ -28,9 +32,9 @@ def contrucaoArvore(text):
             frequencia[char] = 1
 
     #Criar uma lista de nó-huffman (valor e frequência)
-    listaNo = [node(char, freq) for char, freq in frequencia.items()]
+    listaNo = [Node(char, freq) for char, freq in frequencia.items()]
     
-    #Transformar a lista em um heapq (árvoreBin/prioridade)
+    #Transformar a lista em um heapq (prioridade) -> pega o menor elemento
     heapq.heapify(listaNo)
 
     #Construir a árvore de Huffman
@@ -40,7 +44,7 @@ def contrucaoArvore(text):
         direita = heapq.heappop(listaNo)
 
         #Crio um nó com valor vazio e com frequência sendo a soma dos outros nós
-        merge = node(None, esquerda.freq + direita.freq)
+        merge = Node(None, esquerda.freq + direita.freq)
         merge.left = esquerda
         merge.right = direita
 
@@ -49,3 +53,31 @@ def contrucaoArvore(text):
     
     #Retornar a árvore já formada
     return listaNo[0]
+
+
+#Criar um dicionário com o codigo de huff para cada valor
+def criarHuffmanCod(no, codigoDaLetra, dicio):
+    if no is None:
+        return
+
+    if no.char is not None:
+        dicio[no.char] = codigoDaLetra
+        return
+
+    criarHuffmanCod(no.left, codigoDaLetra + "0", dicio)
+    criarHuffmanCod(no.right, codigoDaLetra + "1", dicio)
+
+#Cromprimir
+def compactar(dados):
+    arvore = contrucaoArvore(dados)
+    huffmanCodigos = dict()
+    criarHuffmanCod(arvore, "", huffmanCodigos)
+    textoCodificado = "".join(huffmanCodigos[char] for char in dados)
+    return textoCodificado, arvore, huffmanCodigos
+
+#Main
+textao = "ta bad"
+jacod, tree, d = compactar(textao)
+print("Input:", textao)
+print("Compactado:", jacod)
+print("Dicionário e códigos:", d)
